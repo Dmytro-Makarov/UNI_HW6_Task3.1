19211bcf34d658b972e1011793088b36cb3b72c8
Dmytro-Makarov <makarowdoua@gmail.com>
2021-12-01 18:15:56 +0200

    changes

--- src/uni/makarov/hw6/AbstractFactory.java
+++ src/uni/makarov/hw6/AbstractFactory.java
@@ -5,16 +5,16 @@ package uni.makarov.hw6;
 public class AbstractFactory {
 
     public static void main(String[] args) {
-        CarFactory carFactory = new ToyotaFactory();
+        CarFactory carFactory = new FordFactory();
         Car mycar = carFactory.createCar();
         mycar.getInfo();
         Engine myengine = carFactory.createEngine();
         myengine.getPower();
 
         CarFactory carFactory1 = new ToyotaFactory();
-        Car mycar1 = carFactory.createCar();
+        Car mycar1 = carFactory1.createCar();
         mycar1.getInfo();
-        Engine myengine1 = carFactory.createEngine();
+        Engine myengine1 = carFactory1.createEngine();
         myengine1.getPower();
 
         CarFactory carFactory2 = new MersedesFactory();
